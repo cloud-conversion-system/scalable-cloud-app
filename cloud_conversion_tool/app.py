3b76463ede2f775589b08f0db182c57292d65b8e
Juan Alegría <j.alegria@uniandes.edu.co>
2023-04-15 09:11:38 -0500

    Feat: init compression algorithms

--- cloud_conversion_tool/app.py
+++ cloud_conversion_tool/app.py
@@ -1,3 +1,35 @@
 from cloud_conversion_tool import create_app
+from flask import request
+import zipfile
+import py7zr
+import tarfile
 
-app= create_app('cloud_conversion_tool')
+app = create_app('cloud_conversion_tool')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        file = request.files['file']
+        algorithm = request.form['algorithm']
+        filename = file.filename
+        compress_file(filename, algorithm)
+
+
+def compress_file(filename, algorithm):
+    if algorithm == 'zip':
+        with zipfile.ZipFile(filename+'.zip', 'w') as zipf:
+            zipf.write(filename)
+        return f'El archivo {filename} ha sido comprimido con ZIP'
+    elif algorithm == '7z':
+        with py7zr.SevenZipFile(filename+'.7z', 'w') as szf:
+            szf.write(filename)
+        return f'El archivo {filename} ha sido comprimido con 7Z'
+    elif algorithm == 'targz':
+        with tarfile.open(filename+'.tar.gz', 'w:gz') as tgzf:
+            tgzf.add(filename)
+        return f'El archivo {filename} ha sido comprimido con TAR.GZ'
+    elif algorithm == 'tarbz2':
+        with tarfile.open(filename+'.tar.bz2', 'w:bz2') as tbzf:
+            tbzf.add(filename)
+        return f'El archivo {filename} ha sido comprimido con TAR.BZ2'
