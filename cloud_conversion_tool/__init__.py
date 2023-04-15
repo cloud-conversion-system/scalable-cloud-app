6093b43ae781aa64cf11228cd7e65f0ee98dfca2
Juan Alegría <j.alegria@uniandes.edu.co>
2023-04-15 15:17:19 -0500

    Build: update DB URI

--- cloud_conversion_tool/__init__.py
+++ cloud_conversion_tool/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/cloud_conversion_tool'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@ccs-db.cwdug9pwvom0.us-east-1.rds.amazonaws.com/cloud_conversion_tool'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     app.config['JWT_SECRET_KEY'] = 'frase-secreta'
