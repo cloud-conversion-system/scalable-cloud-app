9a461cbab5d7a4d69bbae7f63a3947afa742637b
AndresLugoS <a.lugos@uniandes.edu.co>
2023-04-15 11:53:12 -0500

    Login - signup

--- cloud_conversion_tool/__init__.py
+++ cloud_conversion_tool/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 
 def create_app(config_name):
     app=Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/cloud_conversion_tool'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/cloud_conversion_tool'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.secret_key = 'secret string'
+    
+    app.config['JWT_SECRET_KEY']='frase-secreta'
+    app.config['PROPAGATE_EXCEPTIONS']=True
+
     return app
