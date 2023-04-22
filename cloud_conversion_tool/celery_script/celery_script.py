54455b0e737a956cd0fe3c3369be649d1b3df899
Juan Alegría <j.alegria@uniandes.edu.co>
2023-04-22 15:07:44 -0500

    Fix: update Celery task name

--- cloud_conversion_tool/celery_script/celery_script.py
+++ cloud_conversion_tool/celery_script/celery_script.py
@@ -5,7 +5,7 @@ app.config_from_object('cloud_conversion_tool.celery_script.celery_config')
 
 app.conf.beat_schedule = {
     "run-me-every-ten-seconds":{
-        "task": "tasks.check_database",
+        "task": "cloud_conversion_tool.celery_script.tasks.check_database",
         "schedule": 10.0
     }
 }
