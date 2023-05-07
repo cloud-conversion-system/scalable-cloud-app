afbef7f432eb44c60395dc61ae30f77f85312799
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2023-05-06 19:52:36 -0500

    Fix: Corregir la ruta de archivo

--- cloud_conversion_tool/cloud_bucket_access/gcsManager.py
+++ cloud_conversion_tool/cloud_bucket_access/gcsManager.py
@@ -16,9 +16,8 @@ def uploadFile(file_path, file_name):
 
 
 def downloadFile(file_name):
-    open(FOLDER_PATH+ file_name, 'w').close()  # create empty file
-    blob = bucket.blob(file_name)
-    blob.download_to_filename('/files/' + file_name)
+    blob = bucket.blob('files/' + file_name)
+    blob.download_to_filename(FOLDER_PATH + file_name)
 
 
 def listBlobs(file_name):
