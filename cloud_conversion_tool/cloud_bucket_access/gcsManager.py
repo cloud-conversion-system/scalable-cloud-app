11b0a60fee6139df903fd57804ac00ae26a26422
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2023-05-06 19:42:34 -0500

    Fix: Upload GCP file path to download

--- cloud_conversion_tool/cloud_bucket_access/gcsManager.py
+++ cloud_conversion_tool/cloud_bucket_access/gcsManager.py
@@ -18,7 +18,7 @@ def uploadFile(file_path, file_name):
 def downloadFile(file_name):
     open(FOLDER_PATH+ file_name, 'w').close()  # create empty file
     blob = bucket.blob(file_name)
-    blob.download_to_filename(FOLDER_PATH + file_name)
+    blob.download_to_filename('/files/' + file_name)
 
 
 def listBlobs(file_name):
