6436e6c3abf99817b44baaa7658e3a192add329d
AndresLugoS <a.lugos@uniandes.edu.co>
2023-05-11 18:38:35 -0500

    Fix: run Dockerfile

--- cloud_conversion_tool/worker_pubsub/task.py
+++ cloud_conversion_tool/worker_pubsub/task.py
@@ -3,8 +3,7 @@ import py7zr
 import tarfile
 import os
 
-#from cloud_conversion_tool.celery_script import app
-from cloud_conversion_tool.modelos.modelos import Task, TaskSchema, Status#from ..modelos import Task, TaskSchema, Status
+from ..modelos import Task, TaskSchema, Status
 from ..cloud_bucket_access import gcsManager
 
 from sqlalchemy import create_engine
