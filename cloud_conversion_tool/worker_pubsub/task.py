c12a747783ce6f4b7e98d2d245f09332cd1711cd
AndresLugoS <a.lugos@uniandes.edu.co>
2023-05-11 18:16:34 -0500

    Fix: relative imports

--- cloud_conversion_tool/worker_pubsub/task.py
+++ cloud_conversion_tool/worker_pubsub/task.py
@@ -4,7 +4,7 @@ import tarfile
 import os
 
 #from cloud_conversion_tool.celery_script import app
-from ..modelos import Task, TaskSchema, Status
+from cloud_conversion_tool.modelos.modelos import Task, TaskSchema, Status#from ..modelos import Task, TaskSchema, Status
 from ..cloud_bucket_access import gcsManager
 
 from sqlalchemy import create_engine
